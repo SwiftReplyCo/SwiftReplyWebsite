3311350516c1b8d1e5e33ac1737ca1d9573397cb
Evan Coulson <evan.m.coulson@gmail.com>
2023-02-27 10:55:14 -0800

    Fixed free trial ui logic

--- scripts/free-trial.js
+++ scripts/free-trial.js
@@ -16,17 +16,40 @@ signUpButton.addEventListener('click', function () {
         }),
     })
         .then(function (response) {
-            return response.json();
+            return new Promise(function (resolve, reject) {
+                response
+                    .json()
+                    .then(function (json) {
+                        resolve({
+                            response: response,
+                            json: json,
+                        });
+                    })
+                    .catch(function (error) {
+                        reject(error);
+                    });
+            });
         })
-        .then(() => {
-            toast.style.height = '75px';
-            toast.style.backgroundColor = '#4BB543';
-            toastText.textContent =
-                'Successfully started your 7 day free trial!';
+        .then(function (result) {
+            if (!result.response.ok) {
+                renderErrorToast(result.json.message);
+            } else {
+                renderSuccessToast();
+            }
         })
-        .catch(function (error) {
-            toast.style.height = '75px';
-            toast.style.backgroundColor = '##ff3333';
-            toastText.textContent = 'Something went wrong! Try again later.';
+        .catch(function () {
+            renderErrorToast('Something went wrong! Try again later.');
         });
 });
+
+function renderErrorToast(message) {
+    toast.style.height = '75px';
+    toast.style.backgroundColor = '#ff3333';
+    toastText.textContent = message;
+}
+
+function renderSuccessToast() {
+    toast.style.height = '75px';
+    toast.style.backgroundColor = '#4BB543';
+    toastText.textContent = 'Successfully started your 7 day free trial!';
+}
