893ef4571db46c754c0c0d49da651fc2ca0d85a6
Evan Coulson <evan.m.coulson@gmail.com>
2023-02-27 15:24:03 -0800

    Added info toast to show that request is processing

--- scripts/free-trial.js
+++ scripts/free-trial.js
@@ -3,9 +3,11 @@ const emailInput = document.getElementById('email-input');
 const signUpButton = document.getElementById('sign-up-button');
 const toast = document.getElementById('toast');
 const toastText = document.getElementById('toast-text');
+const loader = document.getElementById('loader');
 
 signUpButton.addEventListener('click', function () {
     const email = emailInput.value;
+    renderInfoToast();
     fetch(endpoint, {
         method: 'POST',
         headers: {
@@ -42,13 +44,22 @@ signUpButton.addEventListener('click', function () {
         });
 });
 
+function renderInfoToast() {
+    loader.style.display = 'block';
+    toast.style.height = '75px';
+    toast.style.backgroundColor = '#54B4D3';
+    toastText.textContent = 'Signing you up!';
+}
+
 function renderErrorToast(message) {
+    loader.style.display = 'none';
     toast.style.height = '75px';
     toast.style.backgroundColor = '#ff3333';
     toastText.textContent = message;
 }
 
 function renderSuccessToast() {
+    loader.style.display = 'none';
     toast.style.height = '75px';
     toast.style.backgroundColor = '#4BB543';
     toastText.textContent = 'Successfully started your 7 day free trial!';
