dcce390c5a160d2a669306360c10ae54331ce840
Evan Coulson <evan.m.coulson@gmail.com>
2023-02-27 01:33:39 -0800

    Added toast to free trial

--- scripts/free-trial.js
+++ scripts/free-trial.js
@@ -1,6 +1,8 @@
 const endpoint = 'https://api.swiftreply.co/prod/api/payments/create-trial';
 const emailInput = document.getElementById('email-input');
 const signUpButton = document.getElementById('sign-up-button');
+const toast = document.getElementById('toast');
+const toastText = document.getElementById('toast-text');
 
 signUpButton.addEventListener('click', function () {
     const email = emailInput.value;
@@ -16,10 +18,15 @@ signUpButton.addEventListener('click', function () {
         .then(function (response) {
             return response.json();
         })
-        .then((json) => {
-            console.log(json);
+        .then(() => {
+            toast.style.height = '75px';
+            toast.style.backgroundColor = '#4BB543';
+            toastText.textContent =
+                'Successfully started your 7 day free trial!';
         })
         .catch(function (error) {
-            console.log(error);
+            toast.style.height = '75px';
+            toast.style.backgroundColor = '##ff3333';
+            toastText.textContent = 'Something went wrong! Try again later.';
         });
 });
